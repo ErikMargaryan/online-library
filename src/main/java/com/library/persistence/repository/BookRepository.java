0c0db70768806be55d3b00c05522b56eb28924f1
Erik Margaryan <76230984+ErikMargaryan@users.noreply.github.com>
2023-08-09 05:16:52 +0400

    corrected getBookByGenres method

--- src/main/java/com/library/persistence/repository/BookRepository.java
+++ src/main/java/com/library/persistence/repository/BookRepository.java
@@ -3,12 +3,13 @@ package com.library.persistence.repository;
 import com.library.persistence.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query("SELECT b FROM Book b WHERE b.genre IN :genres")
-    List<Book> getBookByGenres(List<String> genre);
+    @Query(value = "SELECT b FROM Book b WHERE b.genre IN :genres")
+    List<Book> getBookByGenres(@Param("genres") List<String> genre);
 }
