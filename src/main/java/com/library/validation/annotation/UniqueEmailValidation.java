0709b107698b80f6e41d4da161ea6fc805e0e9ac
Erik Margaryan <76230984+ErikMargaryan@users.noreply.github.com>
2023-08-11 05:41:45 +0400

    created annotation for validate duplicate emails

---
+++ src/main/java/com/library/validation/annotation/UniqueEmailValidation.java
@@ -0,0 +1,26 @@
+package com.library.validation.annotation;
+
+import com.library.validation.validator.DuplicateEmailValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+@Target(FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {DuplicateEmailValidator.class})
+public @interface UniqueEmailValidation {
+    //error message
+    public String message() default "This username have already been";
+
+
+    //represents group of constraints
+    public Class<?>[] groups() default {};
+
+    //represents additional information about annotation
+    public Class<? extends Payload>[] payload() default {};
+}
