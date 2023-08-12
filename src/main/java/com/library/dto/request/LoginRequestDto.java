c1595ad572a4fdcaf244c50a2d03bdf02e60b759
Erik Margaryan <76230984+ErikMargaryan@users.noreply.github.com>
2023-08-12 19:22:16 +0400

    added LoginResponseDto

---
+++ src/main/java/com/library/dto/request/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.library.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
