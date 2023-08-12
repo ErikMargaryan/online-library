2e8d03cf02ee743ee66d7688c3478622b5c3cc46
Erik Margaryan <76230984+ErikMargaryan@users.noreply.github.com>
2023-08-12 19:26:26 +0400

    added findRoles using UserRole list

--- src/main/java/com/library/persistence/repository/UserRepository.java
+++ src/main/java/com/library/persistence/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.library.persistence.repository;
 
+import com.library.persistence.entity.Role;
 import com.library.persistence.entity.User;
+import com.library.persistence.entity.joinEntity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    @Query("SELECT ur.role FROM UserRole ur WHERE ur IN :userRoles")
+    List<Role> findRoles(@Param("userRoles") List<UserRole> userRoles);
 }
